18dd598e8df1009d76c2a01ed5634fcb4310d17f
Alexandr Kovalenko <kovalenkoam@gmail.com>
2021-12-02 17:35:53 +0300

    added filer.js

---
+++ filer.js
@@ -0,0 +1,115 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-case-declarations */
+
+((D, B, log = (arg) => console.log(arg)) => {
+    const dropZone = D.querySelector('div')
+    const input = D.querySelector('input')
+    let file
+
+    D.addEventListener('dragover', (ev) => ev.preventDefault())
+    D.addEventListener('drop', (ev) => ev.preventDefault())
+
+    dropZone.addEventListener('click', () => {
+        input.click()
+        input.addEventListener('change', () => {
+            log(input.files)
+            file = input.files[0]
+            handleFile(file)
+        })
+    })
+    let context = []
+    const recordsBP = []
+    let oneStrformFile = ''
+    const handleFile = (file) => {
+        dropZone.remove()
+        input.remove()
+        const allowedFiles = ['SD_workman.xml', 'apbstr.000.txt']
+        if (
+            allowedFiles.indexOf(file.name) == -1
+        ) { return }
+
+        switch (file.name) {
+            case 'SD_workman.xml':
+                {
+                    // createText("DataTreeBP.CSV");
+                    const reader = new FileReader()
+                    reader.readAsText(file, 'UTF-8')
+                    const filename = 'SD_workman.xml';
+                    const codepage = 'UTF-8'
+                    reader.onloadend = async() => {
+                        let content = reader.result;
+                        const url = 'http://8-15604.ds.bapb.internal:8888/api/uploadfilesfromclient/'
+                        try {
+                            const response = await fetch(url, {
+                                method: 'POST', // или 'PUT'
+                                headers: {
+                                    "Accept": "application/json",
+                                    "Content-Type": "application/json",
+                                    "Accept-Encoding": "gzip",
+                                    "Access-Control-Allow-Origin": "*"
+                                },
+                                body: JSON.stringify({
+                                        filename: filename,
+                                        codepage: codepage,
+                                        content: reader.result
+                                    }) // данные могут быть 'строкой' или {объектом}!
+                            })
+                            const json = await response.json()
+                            console.log('Успех:', JSON.stringify(json))
+                        } catch (error) {
+                            console.error('Ошибка:', error)
+                        }
+                    }
+                    const s = 0
+                        // const timer = setTimeout(() => {
+                        //     location.reload()
+                        //     clearTimeout(timer)
+                        // }, 2000)
+                        // break
+                }
+            case 'apbstr.000.txt': // 1251
+                { // createText("DataTreeBP.CSV");
+                    const reader = new FileReader()
+                        // let textArray =
+                    reader.readAsText(file, 'windows-1251')
+
+                    reader.onloadend = async( /* dateStamp */ ) => {
+
+                        context = reader.result
+
+                        try {
+                            const url = 'http://localhost:8081/api/ppload'
+                            const response = await fetch(url, {
+                                method: 'POST', // или 'PUT'
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify(ppjson) // данные могут быть 'строкой' или {объектом}!
+
+                            })
+
+                            const json = await response.json()
+                            console.log('Успех:', json)
+                        } catch (error) {
+                            console.error('Ошибка:', error)
+                        }
+                        // return recordsPP;
+                    }
+                    const s = 0
+                    break
+                }
+            default:
+                B.innerHTML = '<h3>Unknown File Format and (or) Filename  !</h3>'
+                const timer = setTimeout(() => {
+                    location.reload()
+                    clearTimeout(timer)
+                }, 2000)
+                break
+        }
+    }
+    const createText = (text) => {
+        const reader = new FileReader()
+        reader.readAsText(text, 'windows-1251')
+        reader.onload = () => (B.innerHTML = `<p><pre>${reader.result}</pre></p>`)
+    }
+})(document, document.body)
